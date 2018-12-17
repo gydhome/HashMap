9843779ac80a76db1d7e4ee7bde0cf75c57c022b
gyd <gyd@localhost.localdomain>
2018-12-18 02:09:55 +0800

    Initialize

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : main.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : HashMap in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
